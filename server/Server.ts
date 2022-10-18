3b852aada28580cedd67f176ad3d15ce38a030e4
jakub-malec826 <jakub.malec826@gmail.com>
2022-10-18 23:30:13 +0200

    Changes to be committed: 	modified:   server/Server.ts

--- server/Server.ts
+++ server/Server.ts
@@ -19,9 +19,9 @@ server.use("/users", usersRouter);
 server.use("/admin", adminRouter);
 
 process.env.NODE_ENV === "development"
-    ? server.listen(9090, "192.168.0.145", () =>
-          console.log("App starts on port", 9090)
-      )
-    : server.listen(process.env.PORT, () =>
-          console.log("App starts on port", process.env.PORT)
-      );
+	? server.listen(9090, "192.168.0.145", () =>
+			console.log("App starts on port", 9090)
+	  )
+	: server.listen(process.env.PORT, () =>
+			console.log("App starts on port", process.env.PORT)
+	  );

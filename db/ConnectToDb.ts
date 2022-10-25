4a17d454c9579ab78bc71e286691dd9d70ee692b
jakub-malec826 <jakub.malec826@gmail.com>
2022-10-25 14:57:35 +0200

    c

--- db/ConnectToDb.ts
+++ db/ConnectToDb.ts
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const url = `mongodb+srv://${process.env.MONGO_URL_DATA}.4tqbyhs.mongodb.net/?retryWrites=true&w=majority`;
+const url =
+	"mongodb+srv://jakub-malec826:P6XgOum8RR6siL6a@usersdb.4tqbyhs.mongodb.net/?retryWrites=true&w=majority";
 
 export default function ConnectToDb() {
-    try {
-        mongoose.connect(url);
-        console.log("Connection with db OK");
-    } catch (err) {
-        console.error(err);
-    }
+	try {
+		mongoose.connect(url);
+		console.log("Connection with db OK");
+	} catch (err) {
+		console.error(err);
+	}
 }
